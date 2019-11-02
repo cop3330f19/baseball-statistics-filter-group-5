edcf5b37844ef8b44e46c2015345fe4034056385
slafrance22 <56932450+slafrance22@users.noreply.github.com>
2019-11-02 18:39:10 -0400

    Add files via upload

---
+++ group-project-1/Date.h
@@ -0,0 +1,55 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Date
+{
+	
+	private:
+		const int day, month, year;
+        int birthMonth,birthDay,birthYear;
+	
+	public:
+		//Constructor accepts the date in the format YYYY, MM, DD
+		//eg Date date(2019, 9, 27);
+		explicit Date(int y, int m, int d):
+			year(y), month(m), day(d){}
+
+		Date(int bmonth,int bday,int byear);
+    
+		int getDay()const; //Returns the day
+            {return day;}
+		int getMonth()const; //Returns the month
+            {return month;}
+		int getYear()const;//Returns the year
+            {return year;}
+		std::string getDate()const;
+    
+    //Person's Birth Date
+        int getBirthDay()const; //Returns the day
+            {return birthDay;}
+		int getBirthMonth()const;//Returns the month
+            {return birthMonth;}
+		int getBirthYear()const; //Returns the year
+            {return birthYear;}
+		int getAge()const; //Calculates age in years
+         { 
+             int A=0;
+
+             if(getBirthMonth() > getMonth())
+               {
+                A = 1;   
+               }
+             if((getBirthMonth() == getMonth()) && (getBirthDay() > getDay()))
+               {
+                A = 1;   
+               }
+
+              return (getYear()-getYear()) - A;
+        }
+      
+};
+#endif
