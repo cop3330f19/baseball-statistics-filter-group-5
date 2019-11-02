a75e1f5c97ded34ea23537e1932cb82cd773ce3b
Darrell1017 <57148454+Darrell1017@users.noreply.github.com>
2019-11-02 17:59:50 -0400

    Update BaseballStatistic.cpp

--- BaseballStatistic.cpp
+++ BaseballStatistic.cpp
@@ -4,3 +4,23 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+
+using namespace std;
+
+BaseballStatistic::BaseballStatistic(string fname, string lname, string tname, int jnum, string pos, char bat, char throw, int atbats, double bAV, int HR, int rbi, int sb, double ops, double era)
+{
+    setFirstName(fname);
+    setLastName(lname);
+    setTeamName(tname);
+    setJerseyNum(jnum);
+    setPosition(pos);
+    setBatting(bat);
+    setThrowing(throw);
+    setAtBats(atbats);
+    setBattingAverage(bAV);
+    setHR(HR);
+    setRBI(rbi);
+    setSB(sb);
+    setOPS(ops);
+    setERA(era); 
+}
