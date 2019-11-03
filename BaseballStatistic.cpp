dabe078956257dba5ad8505372fd92283a0f68b8
Taris Green <54614217+Neverfugazi2019@users.noreply.github.com>
2019-11-02 20:10:06 -0400

    Update BaseballStatistic.cpp

--- BaseballStatistic.cpp
+++ BaseballStatistic.cpp
@@ -16,62 +16,62 @@ BaseballStatistic::BaseballStatistic(string fname, string lname, string tname, i
         position(pos), batting(bat), throwing(throw), atBats(atbats), battingAverage(bAV), HR(hr), RBI(rbi), SB(sb), OPS(ops), ERA(era){
 }
 
-void BaseballStatistic::setFirstName
+void BaseballStatistic::setFirstName(string fname)
 {
-
+   firstName = fname;
 }
 
-void BaseballStatistic::setLastName
+void BaseballStatistic::setLastName(string lname)
 {
-
+   lastName = lname;
 }
-void BaseballStatistic::setTeamName
+void BaseballStatistic::setTeamName(string tname)
 {
-
+   teamName = tname;
 }
-void BaseballStatistic::setJerseyNum
+void BaseballStatistic::setJerseyNum(int jnum)
 {
-
+   jerseyNum = jnum;
 }
-void BaseballStatistic::setPosition
+void BaseballStatistic::setPosition(string pos)
 {
-
+   position = pos;
 }
-void BaseballStatistic::setBatting
+void BaseballStatistic::setBatting(char bat)
 {
-
+   batting = bat;
 }
-void BaseballStatistic::setThrowing
+void BaseballStatistic::setThrowing(char throw)
 {
-
+   throwing = throw;
 }
-void BaseballStatistic::setAtBats
+void BaseballStatistic::setAtBats(int atbats)
 {
-
+   atBats = atbats;
 }
-void BaseballStatistic::setBattingAverage
+void BaseballStatistic::setBattingAverage(double bAV)
 {
-
+   battingAverage = bAV;
 }
-void BaseballStatistic::setHR
+void BaseballStatistic::setHR(int hr)
 {
-
+  HR = hr;
 }
-void BaseballStatistic::setRBI
+void BaseballStatistic::setRBI(int rbi)
 {
-
+  RBI = rbi;
 }
-void BaseballStatistic::setSB
+void BaseballStatistic::setSB(int sb)
 {
-
+  SB = sb;
 }
-void BaseballStatistic::setOPS
+void BaseballStatistic::setOPS(double ops)
 {
-
+  OPS = ops;
 }
-void BaseballStatistic::setERA
+void BaseballStatistic::setERA(double era)
 {
-
+  ERA = era;
 }
 
 string BaseballStatistic::getFirstName()const
