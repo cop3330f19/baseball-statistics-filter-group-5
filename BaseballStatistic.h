004f78b8410ba8bf56f004b1bf535aeea36fc5f4
Darrell1017 <57148454+Darrell1017@users.noreply.github.com>
2019-11-05 03:28:14 -0500

    Update BaseballStatistic.h

--- BaseballStatistic.h
+++ BaseballStatistic.h
@@ -4,75 +4,76 @@
 #include "Date.h"
 #include <string>
 
+
 class BaseballStatistic
 {
-	
-	private:
-		std::string firstName,
-			lastName,
-			teamName,
-			position;
-		
-		int jerseyNum,
-			atBats,
-			hr,
-			rbi,
-			sb;
-		
-		char batting,
-			throwing;
-		
-		double battingAverage,
-			ops,
-			era;
-			
-		Date * dob;
-		
-	public:
-		BaseballStatistic(std::string, std::string, std::string, int,
-			std::string, int, int, int, char, char, int, double, int,
-			int, int, double, double);
-			
+    
+    private:
+        std::string firstName,
+            lastName,
+            teamName,
+            position;
+        
+        int jerseyNum,
+            atBats,
+            hr,
+            rbi,
+            sb;
+        
+        char batting,
+            throwing;
+        
+        double battingAverage,
+            ops,
+            era;
+            
+        Date * dob;
+        
+    public:
+        BaseballStatistic(std::string, std::string, std::string, int,
+            std::string, int, int, int, char, char, int, double, int,
+            int, int, double, double);
+            
 
-		
-		//mutator
-		void setfirstName(std::string);
-		void setlastName(std::string);
-		void setteamName(std::string);
-		void setjerseyNum(int);
-		void setposition(std::string);
-		void setbatting(char);
-		void setthrowing(char);
-		void setatBats(int);
-		void setbattingAverage(double);
-		void sethr(int);
-		void setrbi(int);
-		void setsb(int);
-		void setops(double);
-		void setera(double);
-		
-		
-		//accessor
-		std::string getfirstName()const;
-		std::string getlastName()const;
-		std::string getteamName()const;
-		int getjerseyNum()const;
-		std::string getposition()const;
-		int getAge()const;
-		char getbatting()const;
-		char getthrowing()const;
-		int getatBats()const;
-		double getbattingAverage()const;
-		int gethr()const;
-		int getrbi()const;
-		int getsb()const;
-		double getops()const;
-		double getera()const;
-		
-		//prints a single statistic row to the terminal
-		void print()const;
-		
-		
+        
+        //mutator
+        void setFirstName(std::string);
+        void setLastName(std::string);
+        void setTeamName(std::string);
+        void setJerseyNum(int);
+        void setPosition(std::string);
+        void setBatting(char);
+        void setThrowing(char);
+        void setAtBats(int);
+        void setBattingAverage(double);
+        void setHR(int);
+        void setRBI(int);
+        void setSB(int);
+        void setOPS(double);
+        void setERA(double);
+        
+        
+        //accessor
+        std::string getFirstName()const;
+        std::string getLastName()const;
+        std::string getTeamName()const;
+        int getJerseyNum()const;
+        std::string getPosition()const;
+        int getAge()const;
+        char getBatting()const;
+        char getThrowing()const;
+        int getAtBats()const;
+        double getBattingAverage()const;
+        int getHR()const;
+        int getRBI()const;
+        int getSB()const;
+        double getOPS()const;
+        double getERA()const;
+        
+        //prints a single statistic row to the terminal
+        void print()const;
+        
+        
 };
 
 #endif
